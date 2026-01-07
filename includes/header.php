3b7cbe2ecc45d5a8a92c81e8e1fe8a5d0183353b
Municipalidad Los Lagos <informatica@muniloslagos.cl>
2026-01-07 17:23:34 -0300

    Add cache busting to CSS file

--- includes/header.php
+++ includes/header.php
@@ -28,7 +28,7 @@ $current_profile = $is_logged_in && isset($_SESSION['profile']) ? $_SESSION['pro
     <title><?php echo SITE_NAME; ?></title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.0/font/bootstrap-icons.css" rel="stylesheet">
-    <link href="<?php echo SITE_URL; ?>css/style.css" rel="stylesheet">
+    <link href="<?php echo SITE_URL; ?>css/style.css?v=<?php echo time(); ?>" rel="stylesheet">
 </head>
 <body>
     <!-- Navbar Mejorada -->
